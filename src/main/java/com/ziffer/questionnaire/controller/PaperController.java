086a506dd457bd30552ddf0122cd9f2ea5dbf377
sfofgalaxy <3170105860@zju.edu.cn>
2020-05-12 20:16:57 +0800

    tests finished

--- src/main/java/com/ziffer/questionnaire/controller/PaperController.java
+++ src/main/java/com/ziffer/questionnaire/controller/PaperController.java
@@ -3,15 +3,14 @@ package com.ziffer.questionnaire.controller;
 import com.ziffer.questionnaire.dto.GeneralMessage;
 import com.ziffer.questionnaire.intercepter.AuthToken;
 import com.ziffer.questionnaire.mapper.PaperDao;
+import com.ziffer.questionnaire.model.Paper;
 import com.ziffer.questionnaire.model.Question;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/paper")
@@ -20,22 +19,24 @@ public class PaperController {
     private PaperDao paperDao;
     @RequestMapping(value = "/post",method = RequestMethod.POST)
     @AuthToken
-    public GeneralMessage postPaper(@RequestParam("username") String username,
-                                    @RequestParam("title") String title,
-                                    @RequestParam("description") String description,
-                                    @RequestParam("mode") Byte mode,
-                                    @RequestParam("fillNumber") Integer fillNumber,
-                                    @RequestParam("start") Date start,
-                                    @RequestParam("end") Date end,
+    public GeneralMessage postPaper(Paper paper,
                                     @RequestParam("question") List<Question> questionList){
         GeneralMessage message = new GeneralMessage();
         System.out.println(questionList);
         return message;
     }
     @RequestMapping(value = "/test",method = RequestMethod.POST)
-    public GeneralMessage test(@RequestParam("question") List<Question> questionList){
+    public GeneralMessage test(@RequestParam("question") String params){
         GeneralMessage message = new GeneralMessage();
-        System.out.println(questionList);
+        System.out.println(params);
+        //传输的为：
+//        let data = [
+//                  [1,1,1,1],
+//                  [2,1,1,1],
+//                  [3,1,1,1],
+//                  [4,1,1,1]
+//            ];
+        //接收的参数形式为1,1,1,1,2,1,1,1,3,1,1,1,4,1,1,1
         return message;
     }
 }
